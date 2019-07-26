2dfe9494042c47b362220956070fb8aae3818516
Chi-Yun-Lin <kvln268426@gmail.com>
2019-07-26 07:11:04 +0000

    Add edge_traffic_warn

--- exercises/dns_amplification/controller_gui.py
+++ exercises/dns_amplification/controller_gui.py
@@ -6,9 +6,12 @@ import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
 import threading
+from event import myEvent
+import random
 
 g_height = 1000
-g_width = 500
+g_width = 700
+rpktThreshold = 20000
 
 
 class ControllerGui():
@@ -23,30 +26,37 @@ class ControllerGui():
         self.fonts = ("arial", 12, "bold")
 
         self.var = StringVar()
-        self.L1 = Label(self.root, textvariable=self.var, width=30, height=2)
-        self.L1.pack(side = LEFT )
+        self.L1 = Label(self.root, textvariable=self.var, width=55, height=2)
+        self.L1.place(x=120, y=500)
 
+        #self.tree = ttk.Treeview(self.root, columns=('col1', 'col2', 'col3', 'col4') ,show='headings')
         self.tree = ttk.Treeview(self.root, columns=('col1', 'col2', 'col3') ,show='headings')
         self.tree.column('col1', width=150, anchor='center')
         self.tree.column('col2', width=150, anchor='center')
         self.tree.column('col3', width=50, anchor='center')
+        #self.tree.column('col4', width=180, anchor='center')
         self.tree.heading('col1', text='mac_addr1')
         self.tree.heading('col2', text='mac_addr2')
         self.tree.heading('col3', text='ID')
+        #self.tree.heading('col4', text='num of packet')
 
         self.sw_mac = sw_mac
         self.h_mac = h_mac
         self.topology = topology
 
         for no, link in sorted(self.topology.items()):
-            self.tree.insert('', no, values=(link.keys()[0], link.keys()[1], no))
+            mac1 = link.keys()[0]
+            mac2 = link.keys()[1]
+            self.tree.insert('', no, values=(mac1, mac2, no))
+            #self.tree.insert('', no, values=(mac1, mac2, no, self.event.getPktNum(mac1, mac2)))
         
         self.tree.bind("<Double-1>", self.dbClick)
 
         self.ybar = ttk.Scrollbar(self.root, orient=VERTICAL, command=self.tree.yview)
         self.tree.configure(yscrollcommand=self.ybar.set)
-        self.tree.place(x=280, y=600)
-        self.ybar.place(x=630, y=600, height=218)
+        self.tree.place(x=100, y=650)
+        self.ybar.place(x=450, y=650, height=218)
+        #self.ybar.place(x=630, y=650, height=218)
 
         self.ge_network()
 
@@ -54,13 +64,16 @@ class ControllerGui():
         self.create_node()
 
         self.button_quit = Button(self.root, text="Quit", fg='white', bg='red', font=self.fonts, command=self.quit)
-        self.button_quit.place(x=130, y=450)
+        self.button_quit.place(x=500, y=830)
 
         self.button_refresh = Button(self.root, text="Refresh", fg='white', bg='green', font=self.fonts, command=self.refresh_network)
-        self.button_refresh.place(x = 130, y = 420)
+        self.button_refresh.place(x=500, y=800)
 
         self.cv.pack()
         self.cv.bind('<Motion>' , self.move_handler)
+
+        self.edgeWarn_th = threading.Thread(target=self.edge_traffic_warn)
+        self.edgeWarn_th.start()
 
         self.root.mainloop()
 
@@ -86,31 +99,40 @@ class ControllerGui():
         
 
     def refresh_network(self):
-        """refresh network """
+        """ refresh network """
 
         self.G.clear()
         self.ge_network()
 
         self.cv.delete("all")
         self.create_node()
+        #for no, link in sorted(self.topology.items()):
+        #    mac1 = link.keys()[0]
+        #    mac2 = link.keys()[1]
+        #    pktNum = random.randint(1, 20)
+        #    self.cv.itemconfig(self.event.getObjID(mac1, mac2), width=pktNum, fill="red")
 
     def create_node(self):
         """ create node """
-        img_sw = Image.open("Img/switch.png").resize((40, 40), Image.ANTIALIAS)
+        #img_sw = Image.open("Img/switch.png").resize((40, 40), Image.ANTIALIAS)
         # img_ctr = Image.open("Img/controller.png").resize((40, 40), Image.ANTIALIAS)
-        img_host = Image.open("Img/host.png").resize((40, 40), Image.ANTIALIAS)
+        #img_host = Image.open("Img/host.png").resize((40, 40), Image.ANTIALIAS)
         # img_pkt = Image.open("Img/packet.png").resize((40, 40), Image.ANTIALIAS)
-        self.photo_sw = ImageTk.PhotoImage(img_sw)
+        #self.photo_sw = ImageTk.PhotoImage(img_sw)
         # self.photo_ctr = ImageTk.PhotoImage(img_ctr)
-        self.photo_host = ImageTk.PhotoImage(img_host)
+        #self.photo_host = ImageTk.PhotoImage(img_host)
         # self.photo_pkt = ImageTk.PhotoImage(img_pkt)
 
         for node, pos in self.nodes.items():
             pos[0] = (self.extend(pos[0], 'x')+2)*125
             pos[1] = (self.extend(pos[1], 'y')+2)*125
 
+        self.linkID = []
+
         for link in self.links:
-            self.cv.create_line(self.nodes[link[0]][0]+self.node_size/2, self.nodes[link[0]][1]+self.node_size/2, self.nodes[link[1]][0]+self.node_size/2, self.nodes[link[1]][1]+self.node_size/2)
+            No = self.cv.create_line(self.nodes[link[0]][0]+self.node_size/2, self.nodes[link[0]][1]+self.node_size/2, self.nodes[link[1]][0]+self.node_size/2, self.nodes[link[1]][1]+self.node_size/2)
+            self.linkID.append(No)
+            self.event.putObjID(No, link[0], link[1])
 
         self.switches = []
         self.hosts = []
@@ -125,13 +147,24 @@ class ControllerGui():
                 self.hosts.append(host)
 
     def extend(self, num, axis='x'):
+        """ expand network size """
         if num < 0:
             num = abs(num)
             return sqrt(num) * sqrt(2) * (-1)
         else:
             return sqrt(num) * sqrt(2)
 
+    def edge_traffic_warn(self):
+        """ detect which edge is busy, warn user via color changing """
+        for no, link in sorted(self.topology.items()):
+            mac1 = link.keys()[0]
+            mac2 = link.keys()[1]
+            pktNum = self.event.getPktNum(mac1, mac2)
+            if pktNum > rpktThreshold:
+                self.cv.itemconfig(self.event.getObjID(mac1, mac2), width=pktNum, fill="red")
+
     def dbClick(self, event):
+        """ double click one row """
         self.item = self.tree.selection()[0]
         print "you clicked on ", self.tree.item(self.item, "values")
                 
@@ -144,12 +177,17 @@ class ControllerGui():
         # exit()
 
     def move_handler(self, event):
+        """ detect if mouse is focus, show information """
         self.var.set('')
         
         for node, pos in self.nodes.items():
             if  pos[0] < event.x < pos[0]+self.node_size and pos[1] < event.y < pos[1]+self.node_size:
-                self.var.set(node)
+                if node[15:] == "00" :
+                    self.var.set("Switch Mac: "+node)
+                else:
+                    self.var.set("Host Mac: "+node)
                 break
+            #elif 
 
 def main():
 
@@ -159,7 +197,7 @@ def main():
 
     topology = {'24': {'00:00:00:04:15:00': 1, '00:00:00:0b:15:00': 2}, '25': {'00:00:00:04:15:00': 2, '00:00:00:0c:15:00': 2}, '26': {'00:00:00:04:15:00': 3, '00:00:00:0d:15:00': 2}, '27': {'00:00:00:0e:15:00': 2, '00:00:00:04:15:00': 4}, '20': {'00:00:00:07:15:00': 2, '00:00:00:04:15:00': 12}, '21': {'00:00:00:08:15:00': 2, '00:00:00:04:15:00': 13}, '22': {'00:00:00:09:15:00': 2, '00:00:00:04:15:00': 14}, '23': {'00:00:00:0a:15:00': 2, '00:00:00:09:15:00': 1}, '28': {'00:00:00:0f:15:00': 2, '00:00:00:04:15:00': 5}, '29': {'00:00:00:10:15:00': 2, '00:00:00:04:15:00': 6}, '1': {u'00:00:00:00:12:0d': 1, '00:00:00:12:15:00': 1}, '0': {'00:00:00:13:15:00': 1, u'00:00:00:00:13:0e': 1}, '3': {'00:00:00:0d:15:00': 1, u'00:00:00:00:0d:09': 1}, '2': {'00:00:00:08:15:00': 1, u'00:00:00:00:08:05': 1}, '5': {'00:00:00:01:15:00': 1, u'00:00:00:00:01:01': 1}, '4': {u'00:00:00:00:0c:08': 1, '00:00:00:0c:15:00': 1}, '7': {'00:00:00:07:15:00': 1, u'00:00:00:00:07:04': 1}, '6': {'00:00:00:0a:15:00': 1, u'00:00:00:00:0a:06': 1}, '9': {u'00:00:00:00:0f:0b': 1, '00:00:00:0f:15:00': 1}, '8': {u'00:00:00:00:10:0c': 1, '00:00:00:10:15:00': 1}, '11': {u'00:00:00:00:03:03': 1, '00:00:00:03:15:00': 1}, '10': {'00:00:00:0e:15:00': 1, u'00:00:00:00:0e:0a': 1}, '13': {u'00:00:00:00:02:02': 1, '00:00:00:02:15:00': 1}, '12': {u'00:00:00:00:15:0f': 1, '00:00:00:15:15:00': 1}, '15': {'00:00:00:05:15:00': 1, '00:00:00:02:15:00': 2}, '14': {u'00:00:00:00:0b:07': 1, '00:00:00:0b:15:00': 1}, '17': {'00:00:00:05:15:00': 3, '00:00:00:04:15:00': 10}, '16': {'00:00:00:05:15:00': 2, '00:00:00:03:15:00': 2}, '19': {'00:00:00:06:15:00': 2, '00:00:00:04:15:00': 11}, '18': {'00:00:00:01:15:00': 2, '00:00:00:06:15:00': 1}, '31': {'00:00:00:11:15:00': 1, '00:00:00:12:15:00': 2}, '30': {'00:00:00:11:15:00': 2, '00:00:00:04:15:00': 7}, '34': {'00:00:00:14:15:00': 1, '00:00:00:15:15:00': 2}, '33': {'00:00:00:04:15:00': 9, '00:00:00:14:15:00': 2}, '32': {'00:00:00:04:15:00': 8, '00:00:00:13:15:00': 2}}
 
-    event = threading.Event()
+    event = myEvent(topology)
     c = ControllerGui(event, sw_mac, h_mac, topology)
 if __name__ == '__main__':
     main()
